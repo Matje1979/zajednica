880be35612e03433acd5707a675613e692c3016b
S7Hudi Dev <s7hudidev01@s7codedesign.com>
2022-03-19 15:15:27 +0100

    various changes

--- home_manager/asgi.py
+++ home_manager/asgi.py
@@ -1,13 +1,15 @@
 import os
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+# from django.core.asgi import get_asgi_application
+from channels.http import AsgiHandler
+
 import home.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home_manager.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": AsgiHandler(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             home.routing.websocket_urlpatterns
