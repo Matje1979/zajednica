d23a2b8f128aac82fc08c0d6078643004ef53175
Matje1979 <ibnruzd@yahoo.com>
2022-06-23 20:32:39 +0200

    settings static modified

--- home_manager/asgi.py
+++ home_manager/asgi.py
@@ -1,19 +1,39 @@
+# import os
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# # from django.core.asgi import get_asgi_application
+# from channels.http import AsgiHandler
+
+# import home.routing
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home_manager.settings')
+
+# application = ProtocolTypeRouter({
+#     "http": AsgiHandler(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             home.routing.websocket_urlpatterns
+#         )
+#     ),
+#     # Just HTTP for now. (We can add other protocols later.)
+# })
+
 import os
-from channels.auth import AuthMiddlewareStack
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
-# from django.core.asgi import get_asgi_application
-from channels.http import AsgiHandler
+from django.core.asgi import get_asgi_application
 
-import home.routing
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "channels_celery_heroku_project.settings"
+)
+django.setup()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'home_manager.settings')
+from channels.auth import AuthMiddleware, AuthMiddlewareStack
+from notifications_app.routing import websocket_urlpatterns
 
-application = ProtocolTypeRouter({
-    "http": AsgiHandler(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            home.routing.websocket_urlpatterns
-        )
-    ),
-    # Just HTTP for now. (We can add other protocols later.)
-})
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(URLRouter(websocket_urlpatterns)),
+    }
+)
